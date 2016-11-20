6308694329c1db1e064611d1eb06aa8731e9dd86
SidneyW <sidneywijngaarde@gmail.com>
2016-11-20 00:21:00 -0500

    Gave up on cube mapping

--- js/main.js
+++ js/main.js
@@ -74,13 +74,30 @@ function init() {
   waterMinHeight = 75;
   waterSetVertices(waterVertices, waterData, waterMinHeight);
 
-  waterMesh = new THREE.Mesh( waterGeometry, new THREE.MeshBasicMaterial(
-  {color: 0x42A5F5,
-   transparent: true,
-   opacity: 0.9}));
+  var material = new THREE.ShaderMaterial({
+                                        uniforms: {
+                                          resolution: {value: new THREE.Vector2(512, 512)},
+                                          cameraPos: {value: camera.getWorldPosition()},
+                                          cameraDir: {value: camera.getWorldDirection()}
+                                        },
+                                        vertexShader: document.getElementById( 'vertex_shader' ).textContent,
+                                        fragmentShader: document.getElementById( 'fragment_shader').textContent
+                                      });
+
+  /* waterMesh = new THREE.Mesh( waterGeometry, new THREE.MeshBasicMaterial( */
+  /*   { color: 0x42A5F5, */
+  /*     transparent: true, */
+  /*     opacity: 0.9 }) */
+  /* ); */
+
+
+  waterMesh = new THREE.Mesh(waterGeometry, material);
+
   scene.add( waterMesh );
 
+
   renderer = new THREE.WebGLRenderer();
+  canvas = renderer.domElement;
   renderer.setClearColor( 0xbfd1e5 );
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
